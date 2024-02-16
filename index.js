6d9b27c42a1e42ed1161c9f701aba3ca91a7e7a9
Waffle_1415 <purinpurinmituhide@gmail.com>
2024-02-17 01:40:20 +0900

    fix

--- index.js
+++ index.js
@@ -17,15 +17,6 @@ const app = express();
 
 const { exec } = require('child_process');
 
-exec('npm install discord.js', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
-
 const discord_api = axios.create({
   baseURL: 'https://discord.com/api/',
   timeout: 30000,
